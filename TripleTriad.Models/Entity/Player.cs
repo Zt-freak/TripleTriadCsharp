3bdbc9aee022287d1f1b51d0302b3fedf7dc79f9
Apodemus Erectus <bnkho98@hotmail.com>
2021-08-23 15:35:08 +0200

    install EF

--- TripleTriad.Models/Entity/Player.cs
+++ TripleTriad.Models/Entity/Player.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using TripleTriad.Models.Entity.Interface;
 
 namespace TripleTriad.Models.Entity
 {
     public class Player : IPlayer
     {
+        public Player ()
+        {
+
+        }
+        public Player (IEnumerable<ICard> cards)
+        {
+            Deck = cards;
+        }
         public int Id { get; set; }
         public Color TeamColor { get; set; }
         public IEnumerable<ICard> Deck { get; set; }
